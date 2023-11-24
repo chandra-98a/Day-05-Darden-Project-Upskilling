ae1f5f06ebb18f37e4883586bff835a20d3ae4eb
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-24 12:36:50 +0530

    "@PostMapping,@PutMapping

--- SpringBootRestAPI/src/main/java/com/capgemini/SpringBootRestAPI/controller/StudentController.java
+++ SpringBootRestAPI/src/main/java/com/capgemini/SpringBootRestAPI/controller/StudentController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +45,21 @@ public class StudentController {
                                           @RequestParam String fName,
                                           @RequestParam String lName){
         return new Student(id, fName, lName);
+    }
+    //@RequestBody @PostMapping
+    @PostMapping("/students/create")
+    public  Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getfName());
+        System.out.println(student.getlName());
+return student;
+    }
+    
+    //@PutMapping
+    @PutMapping("/students/{id}/update")
+    public Student updateStudent(@RequestBody Student student,@PathVariable("id") int studentId) {
+    	System.out.println(student.getfName());
+    	System.out.println(student.getlName());
+    	return student;
     }
 }
